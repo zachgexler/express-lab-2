d6bcd622f18ee52a46b07d74226caa7d13813cac
Zachary Gexler <zacharygexler@Zacharys-MacBook-Pro.local>
2022-08-29 19:28:30 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -16,14 +16,20 @@ const app = express();
 //     res.send("Hello Stranger")
 //   })
 
-
+//Greeting
 app.get("/greeting/:name", function(req, res){
     res.send("What's Up " + req.params.name);
   })
 
+//Tip Calculator
+//http://localhost:3000/tip/100/20
+app.get("/tip/:total/:tipPercentage", function (req, res) {
 
+    res.send("Your Recommended Tip " + req.params["total", "tipPercentage"]
+    )
+})
   
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("express is listening for requests from the browser")
 })
 
